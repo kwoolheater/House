bf1eb2eb404e08e743db22ae5b9c30a9845cf417
Kiyoshi Woolheater <kiyoshiwoolheater@gmail.com>
2017-09-02 16:32:10 -0400

    Initial Commit

---
+++ House/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  House
+//
+//  Created by Kiyoshi Woolheater on 9/2/17.
+//  Copyright © 2017 Kiyoshi Woolheater. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
